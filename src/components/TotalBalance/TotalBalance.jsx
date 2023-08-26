965c8b0c79a94f855d52b93afcad1220b6824e11
Patrício Samuel <patricioanjosjf@gmail.com>
2023-08-26 15:50:59 -0300

    commit

--- src/components/TotalBalance/TotalBalance.jsx
+++ src/components/TotalBalance/TotalBalance.jsx
@@ -10,7 +10,7 @@ export default function TotalBalance({onClick, totalBalance, className}) {
     return (
         <div onClick={onClick} className={`container ${className}`}>
             <div className={className}>
-                <img src="/app-wallet-passes.png" alt="ícone" />
+                <img src="images/app-wallet-passes.png" alt="ícone" />
             </div>
             <p>Total</p>
             <h2>R${totalBalance}</h2>
--- src/components/TotalSaved/TotalSaved.jsx
+++ src/components/TotalSaved/TotalSaved.jsx
@@ -10,7 +10,7 @@ export default function TotalSaved({onClick, incomes, className}) {
     return (
         <div onClick={onClick} className={`container ${className}`}>
             <div className={className}>
-                <img src="/money-bag.png" alt="ícone" />
+                <img src="images/money-bag.png" alt="ícone" />
             </div>
             <p>Receitas Totais</p>
             <h2 style={{color: 'green'}}>R${incomes}</h2>
